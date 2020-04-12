cc82fc5ff1623de9238a461e23c59e0bf475e299
kelibst <kbooster17@gmail.com>
2020-04-12 16:47:44 +0000

    winning function added

--- bin/main.rb
+++ bin/main.rb
@@ -1,18 +1,8 @@
 #!/usr/bin/env ruby
 require_relative '../lib/user_interface'
-winning_Steps = [ 
-[1,2,3], # top_row 
-[4,5,6], # middle_row 
-[7,8,9], # bottom_row 
-[1,4,7], # left_column 
-[2,5,8], # center_column 
-[3,6,9], # right_column 
-[1,5,9], # left_diagonal 
-[3,5,7] # right_diagonal 
-].freeze
 
-player1_history = []
-player2_history = []
+
+
 
 def start_game(play = '')
   res = false
@@ -48,8 +38,6 @@ end
   player_hash =get_player_name
 
 
-
-
 dashboard_arr = %w[1 2 3 4 5 6 7 8 9]
 def make_move(player, input = '')
   
@@ -61,17 +49,55 @@ def make_move(player, input = '')
   input
 end
 
-def valid_move?(player, dashboard_arr, player_hash, input = '')
+def check_win(player, player_history)
+  winning_Steps = [ 
+[1,2,3], # top_row 
+[4,5,6], # middle_row 
+[7,8,9], # bottom_row 
+[1,4,7], # left_column 
+[2,5,8], # center_column 
+[3,6,9], # right_column 
+[1,5,9], # left_diagonal 
+[3,5,7] # right_diagonal 
+].freeze
+  player_history.sort!
+  history_count = 0
+  win = false
+  if player_history.length >=3
+  winning_Steps.each do |x|
+    player_history.each do |y|
+     history_count+=1 if x.include?(y.to_i)
+      win = true if history_count ==3
+    end
+  end
+
+end
+win
+end
+
+def game_over(player)
+  puts "Wow! #{player} Won!"
+end
+
+def valid_move?(player, dashboard_arr, player_hash, input = '',player1_history, player2_history )
+
   input = make_move(player)
   good_move = true
   if dashboard_arr[input.to_i - 1] == "X" || dashboard_arr[input.to_i - 1] =="O"
     good_move = false
   end
   if good_move
+    player_history = player1_history if player == player_hash["X"]
+    player_history = player2_history if player == player_hash["O"]
+    player_history.push(input)
+    
     dashboard_arr[input.to_i - 1] = player_hash.key(player)
+    if check_win(player, player_history)
+      game_over(player)
+    end
   else
     puts "#{player}, your move is not valid. Spot taken!"
-    valid_move?(player, dashboard_arr,player_hash)
+    valid_move?(player, dashboard_arr,player_hash, player1_history, player2_history)
   end
   
 end
@@ -79,54 +105,16 @@ end
 
 
 unless player_hash.empty?
+  player1_history = Array.new
+  player2_history = Array.new
         player = ''
         dashboard_arr.each_index do |x|
           can_move = true
            player = player_hash["X"] if x.even?
            player = player_hash["O"] if x.odd?
           
-          valid_move?(player, dashboard_arr, player_hash)
+          valid_move?(player, dashboard_arr, player_hash, player1_history, player2_history)
+         
           puts dashboard(dashboard_arr)
         end  
 end
-
- #  if dashboard_arr[input.to_i - 1] == "X" || dashboard_arr[input.to_i - 1] =="O"
-          #   can_move = false
-          #  else 
-          #   can_move = true
-          #  end
-           
-          #  until can_move == true
-          #   puts "That spot is taken. \n Try Again..."
-          #   input = make_move(player)
-          #   if dashboard_arr[input.to_i - 1] == "X" || dashboard_arr[input.to_i - 1] =="O"
-          #   can_move = false
-          #  else 
-          #   can_move = true
-          #  end
-          #  end
-          
-          
-          
-          #  counter =0
-        #    if can_move 
-        #       dashboard_arr[input.to_i - 1] = player_hash.key(player)
-        #      winning_Steps.each {|x|
-        #      player_history = player1_history if player == player_hash["X"]
-        #      player_history = player2_history if player == player_hash["O"]
-        #      player_history.push(input.to_i)
-        #       x.each{|y| counter+=1 if player_history.include?(y)
-           
-        #     next unless counter ==3}
-        #     }
-        #       player1_history.push(input.to_i) if player == player_hash["X"] 
-        #      player2_history.push(input.to_i) if player == player_hash["O"] 
-
-          
-        #   end
-
-        #  def check_move( player, dashboard_arr, good_move = true)
-  
-#  end
-#  input
-#  end
