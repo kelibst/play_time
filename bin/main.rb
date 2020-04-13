82b0daf118f7b1a9fd5b1b5af97bb78b93daf663
kelibst <kbooster17@gmail.com>
2020-04-13 08:10:46 +0000

    fixed linters

--- bin/main.rb
+++ bin/main.rb
@@ -11,72 +11,55 @@ def start_game(play = '')
   res
 end
 
-def get_player_name(player_hash = Hash.new)
-  player1_name= ""
-  player2_name= ""
-  until !player1_name.empty?
-    puts "Player 1 Enter your name"
+def get_player_name(player_hash = {})
+  player1_name = ''
+  player2_name = ''
+  while player1_name.empty?
+    puts 'Player 1 Enter your name'
     player1_name = gets.chomp
   end
-  player_hash["X"] = player1_name
-  puts "#{player_hash["X"]} your marker is X"
-  
-  until !player2_name.empty?
-    puts "Player 2 Enter your name"
+  player_hash['X'] = player1_name
+  puts "#{player_hash['X']} your marker is X"
+
+  while player2_name.empty?
+    puts 'Player 2 Enter your name'
     player2_name = gets.chomp
   end
-  player_hash["O"] = player2_name
-  puts "#{player_hash["O"]} your marker is O"
+  player_hash['O'] = player2_name
+  puts "#{player_hash['O']} your marker is O"
   player_hash
 end
 
 start_game
-player_hash =get_player_name
+player_hash = get_player_name
 
-game_on = Game_mode.new(player_hash)
+game_on = GameMode.new
 dashboard_arr = %w[1 2 3 4 5 6 7 8 9]
 unless player_hash.empty?
-  player1_history = Array.new
-  player2_history = Array.new
-        player = ''
-        dashboard_arr.each_index do |x|
-          can_move = true
-           player = player_hash["X"] if x.even?
-           player = player_hash["O"] if x.odd?
-          
-          ans = game_on.valid_move?(player, dashboard_arr, player_hash)
-          if ans[0]
-            player_history = player1_history if player == player_hash["X"]
-              player_history = player2_history if player == player_hash["O"]
-              input = ans[1]
-              player_history.push(input)
-              dashboard_arr[input.to_i - 1] = player_hash.key(player)
-            else
-              until ans[0]
-              puts "#{player}, your move is not valid. Spot taken!"
-              ans = game_on.valid_move?(player, dashboard_arr,player_hash)
-              end
-              player_history = player1_history if player == player_hash["X"]
-              player_history = player2_history if player == player_hash["O"]
-              input = ans[1]
-              player_history.push(input)
-              dashboard_arr[input.to_i - 1] = player_hash.key(player)
-          end
-          
-          if game_on.check_win(player, player_history)
-            game_on.game_over(player)
-            puts game_on.dashboard(dashboard_arr)
-            break
-          elsif x== 8
-            puts "Wow! it's a draw."
-        end
-          puts game_on.dashboard(dashboard_arr)
-        end  
+  player1_history = []
+  player2_history = []
+  player = ''
+  dashboard_arr.each_index do |x|
+    player = player_hash['X'] if x.even?
+    player = player_hash['O'] if x.odd?
+
+    ans = game_on.valid_move?(player, dashboard_arr)
+    player_history = player1_history if player == player_hash['X']
+    player_history = player2_history if player == player_hash['O']
+    until ans[0]
+      puts "#{player}, your move is not valid. Spot taken!"
+      ans = game_on.valid_move?(player, dashboard_arr)
+    end
+    input = ans[1]
+    dashboard_arr[input.to_i - 1] = player_hash.key(player)
+    player_history.push(input)
+    if game_on.check_win(player, player_history)
+      game_on.game_over(player)
+      puts game_on.dashboard(dashboard_arr)
+      break
+    elsif x == 8
+      puts "Wow! it's a draw."
+    end
+    puts game_on.dashboard(dashboard_arr)
+  end
 end
-
-
-
-
-
-
-
